36fa4977e5ce5f45ebf0a97a88619fe013ee5147
younes <youn.henni@gmail.com>
2022-12-24 14:57:17 +0100

    Adds express server

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const data = "";
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    }
+);
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+    }
+);
